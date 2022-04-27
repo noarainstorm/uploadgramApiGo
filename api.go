c248f976b1d78856cdf007aa98d232fc8a51e22c
noarainstorm <100447165+noarainstorm@users.noreply.github.com>
2022-04-27 22:10:40 +0300

    finnaly

--- api.go
+++ api.go
@@ -9,35 +9,6 @@ import (
 	"strings"
 )
 
-type Config struct {
-	Api       string
-	UserAgent string
-}
-
-type Response struct {
-	Ok    bool
-	Url   string
-	Token string `json:"delete"`
-
-	DeleteStat int
-}
-
-type DownloadInfo struct {
-	Ok       bool
-	Filename string
-	Size     int
-}
-
-type All struct {
-	File *os.File
-
-	Out []byte
-
-	Config
-	Response
-	DownloadInfo
-}
-
 func New(api string, userAgent string) All {
 	all := All{}
 	if api == "" {
---
+++ types.go
@@ -0,0 +1,30 @@
+package uploadgramApiGo
+
+type Config struct {
+	Api       string
+	UserAgent string
+}
+
+type Response struct {
+	Ok    bool
+	Url   string
+	Token string `json:"delete"`
+
+	DeleteStat int
+}
+
+type DownloadInfo struct {
+	Ok       bool
+	Filename string
+	Size     int
+}
+
+type All struct {
+	File *os.File
+
+	Out []byte
+
+	Config
+	Response
+	DownloadInfo
+}
